b3121b14c8fed464162361fc9fce70c83478dfde
heylarissa <larissah.dandrade@gmail.com>
2023-05-23 18:46:29 -0300

    refatorando código

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,13 @@
 // define o component navbar
 class Navbar extends HTMLElement {
-    
-    constructor() {
+    constructor(open) {
         super();
+        this.open = open;
     }
 
     connectedCallback() {
         this.innerHTML = `
         <div class="sidebar">
-        
             <a href="./index.html"><i class="material-symbols-outlined">home</i>Home</a>
             <a href="./about.html">About</a>
             <a href="./form.html"><i class="material-symbols-outlined">contact_page</i>Contact</a>
@@ -16,6 +15,25 @@ class Navbar extends HTMLElement {
     `;
     }
 
+    openNav() {
+        document.getElementById("sidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+    closeNav() {
+        document.getElementById("sidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
+    moveNav() {
+        this.open = !this.open;
+        if (this.open){
+            this.openNav()
+        }
+        else {
+            this.closeNav()
+        }
+    }
     
 };
 customElements.define('navbar-component', Navbar);
+var nav = new Navbar(false);
+
